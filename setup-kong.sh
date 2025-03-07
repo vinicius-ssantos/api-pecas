1e9247eb6817e5da128cba61d2f208af0ebbe06e
Vinicius <viniciusolicveira@gmail.com>
2025-03-07 11:46:04 -0300

    Atualizando docker-compose.yml e setup-kong.sh

--- setup-kong.sh
+++ setup-kong.sh
@@ -3,17 +3,27 @@
 echo "Aguardando o Kong iniciar..."
 sleep 10  # Aguarda alguns segundos para garantir que o Kong esteja pronto
 
-# Criando um serviço no Kong apontando para a API Spring Boot
-curl -i -X POST --url http://localhost:8001/services/ \
-  --data 'name=api-trabalho' \
-  --data 'url=http://springboot_api:8088'
+# Verificar se o serviço já existe
+if curl -s http://localhost:8001/services/api-trabalho | grep -q '"id"'; then
+  echo "Serviço já existe, pulando criação..."
+else
+  echo "Criando serviço no Kong..."
+  curl -i -X POST --url http://localhost:8001/services/ \
+    --data 'name=api-trabalho' \
+    --data 'url=http://springboot_api:8088'
+fi
 
-# Criando uma rota para a API
-curl -i -X POST --url http://localhost:8001/services/api-trabalho/routes \
-  --data 'paths[]=/api/v1/peca' \
-  --data 'name=peca-route'
+# Verificar se a rota já existe
+if curl -s http://localhost:8001/routes | grep -q '"name":"peca-route"'; then
+  echo "Rota já existe, pulando criação..."
+else
+  echo "Criando rota no Kong..."
+  curl -i -X POST --url http://localhost:8001/services/api-trabalho/routes \
+    --data 'paths[]=/api/v1/peca' \
+    --data 'name=peca-route'
+fi
 
-# Exibe as rotas configuradas
+# Exibir as rotas configuradas
 curl -i -X GET --url http://localhost:8001/routes
 
 echo "Configuração do Kong concluída!"
